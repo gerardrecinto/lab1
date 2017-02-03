7adb0f0f31fda4efbd9ac6ccab27be04ef0693fd
gerardrecinto <grecinto@ucsd.edu>
2017-02-02 21:21:51 -0800

    commit

---
+++ tumblr/PhotoCell.swift
@@ -0,0 +1,26 @@
+//
+//  PhotoCell.swift
+//  tumblr
+//
+//  Created by Gerard Recinto on 2/2/17.
+//  Copyright © 2017 Gerard Recinto. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    @IBOutlet weak var photoImageView: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ tumblr/PhotosViewController.swift
@@ -0,0 +1,92 @@
+//
+//  PhotosViewController.swift
+//  tumblr
+//
+//  Created by Gerard Recinto on 2/2/17.
+//  Copyright © 2017 Gerard Recinto. All rights reserved.
+//
+
+import UIKit
+import AFNetworking
+class PhotosViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var PhotoTableView: UITableView!
+    @IBOutlet weak var PhotoCell: UITableViewCell!
+    
+    var posts: [NSDictionary] = []
+    override func viewDidLoad() {
+        PhotoTableView.rowHeight = 240;
+        PhotoTableView.delegate = self
+        PhotoTableView.dataSource = self
+        super.viewDidLoad()
+        let url = URL(string:"https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")
+        let request = URLRequest(url: url!)
+        let session = URLSession(
+            configuration: URLSessionConfiguration.default,
+            delegate:nil,
+            delegateQueue:OperationQueue.main
+        )
+        
+        let task : URLSessionDataTask = session.dataTask(
+            with: request as URLRequest,
+            completionHandler: { (data, response, error) in
+                if let data = data {
+                    if let responseDictionary = try! JSONSerialization.jsonObject(
+                        with: data, options:[]) as? NSDictionary {
+                        //print("responseDictionary: \(responseDictionary)")
+                        
+                        // Recall there are two fields in the response dictionary, 'meta' and 'response'.
+                        // This is how we get the 'response' field
+                        let responseFieldDictionary = responseDictionary["response"] as! NSDictionary
+                        self.posts = responseFieldDictionary["posts"] as! [NSDictionary]
+                        self.PhotoTableView.reloadData()
+                        // This is where you will store the returned array of posts in your posts property
+                        // self.feeds = responseFieldDictionary["posts"] as! [NSDictionary]
+                    }
+                }
+        });
+        task.resume()
+        
+
+        // Do any additional setup after loading the view.
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PhotoCell") as! PhotoCell
+        //cell?.textLabel?.text = "This is row \(indexPath.row)"
+        let post = posts[indexPath.row]
+        //let photos = post.value(forKeyPath: "photos") as? [NSDictionary]
+        if let photos = post.value(forKeyPath: "photos") as? [NSDictionary] {
+            // photos is NOT nil, go ahead and access element 0 and run the code in the curly braces
+            let imageUrlString = photos[0].value(forKeyPath: "original_size.url") as? String
+            //let imageUrl = URL(string: imageUrlString!)!
+            if let imageUrl = URL(string: imageUrlString!) {
+                // URL(string: imageUrlString!) is NOT nil, go ahead and unwrap it and assign it to imageUrl and run the code in the curly braces
+                cell.photoImageView.setImageWith(imageUrl as URL)
+            }
+
+        }
+        
+        return cell
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
